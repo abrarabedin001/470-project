4cd5ca5ca09001f8b6accc3ea3c7e7d13afe0f9a
abrarabedin001 <abrarabedin001@gmail.com>
2023-10-21 01:40:55 +0600

    removed console log

--- app/settings/layout.tsx
+++ app/settings/layout.tsx
@@ -17,22 +17,22 @@ const sidebarNavItems = [
     title: "Profile",
     href: "/settings",
   },
-  {
-    title: "Account",
-    href: "/settings/account",
-  },
-  {
-    title: "Appearance",
-    href: "/settings/appearance",
-  },
-  {
-    title: "Notifications",
-    href: "/settings/notifications",
-  },
-  {
-    title: "Display",
-    href: "/settings/display",
-  },
+  // {
+  //   title: "Account",
+  //   href: "/settings/account",
+  // },
+  // {
+  //   title: "Appearance",
+  //   href: "/settings/appearance",
+  // },
+  // {
+  //   title: "Notifications",
+  //   href: "/settings/notifications",
+  // },
+  // {
+  //   title: "Display",
+  //   href: "/settings/display",
+  // },
 ]
 
 interface SettingsLayoutProps {

8ac66705853a353cfb30634115d606e43fdfa2f8
abrarabedin001 <abrarabedin001@gmail.com>
2023-10-28 22:22:16 +0600

    fixed the input types in dbfunctions

--- Database/firestore/firebaseDb.ts
+++ Database/firestore/firebaseDb.ts
@@ -43,69 +43,83 @@ export async function createUser(uid: string) {
 
 const bugs = collection(db, 'bugs');
 
-export const logBug = async (details) => {
+interface BugDetails {
+  severity: string;
+  description: string;
+  screenshots?: string[];
+  // any other fields that you want to include
+}
+
+export const logBug = async (details: BugDetails): Promise<string> => {
   const bugDocRef = doc(bugs);
   await setDoc(bugDocRef, { ...details, status: 'Open', createdAt: new Date() });
   return bugDocRef.id;
 }
 
-export const assignBug = async (bugId, assigneeId) => {
+export const assignBug = async (bugId: string, assigneeId: string): Promise<void> => {
   const bugDocRef = doc(bugs, bugId);
   await updateDoc(bugDocRef, { assigneeId });
 }
 
-export const updateBugStatus = async (bugId, status) => {
+export const updateBugStatus = async (bugId: string, status: string): Promise<void> => {
   const bugDocRef = doc(bugs, bugId);
   await updateDoc(bugDocRef, { status });
 }
 
 
-
 const tasks = collection(db, 'tasks');
 
-export const createTask = async (details) => {
+interface TaskDetails {
+  title: string;
+  description: string;
+  priority: string;
+  deadline: Date;
+  // any other fields that you want to include
+}
+
+export const createTask = async (details: TaskDetails): Promise<string> => {
   const taskDocRef = doc(tasks);
   await setDoc(taskDocRef, { ...details, status: 'Open', createdAt: new Date() });
   return taskDocRef.id;
 }
 
-export const assignTask = async (taskId, assigneeId) => {
+export const assignTask = async (taskId: string, assigneeId: string): Promise<void> => {
   const taskDocRef = doc(tasks, taskId);
   await updateDoc(taskDocRef, { assigneeId });
 }
 
-export const updateTask = async (taskId, updates) => {
+export const updateTask = async (taskId: string, updates: Record<string, any>): Promise<void> => {
   const taskDocRef = doc(tasks, taskId);
   await updateDoc(taskDocRef, updates);
 }
 
 const teams = collection(db, 'teams');
 
-export const createTeam = async (teamName, adminId) => {
+export const createTeam = async (teamName: string, adminId: string): Promise<string> => {
   const teamDocRef = doc(teams);
   await setDoc(teamDocRef, { name: teamName, admin: adminId, createdAt: new Date() });
   return teamDocRef.id;
 }
 
-export const addTeamMember = async (teamId, memberId) => {
+export const addTeamMember = async (teamId: string, memberId: string): Promise<void> => {
   const membersCollection = collection(db, 'teams', teamId, 'members');
   const memberDocRef = doc(membersCollection, memberId);
   await setDoc(memberDocRef, { joinedAt: new Date() });
 }
 
-export const removeTeamMember = async (teamId, memberId) => {
+export const removeTeamMember = async (teamId: string, memberId: string): Promise<void> => {
   const memberDocRef = doc(db, 'teams', teamId, 'members', memberId);
   await runTransaction(db, async (transaction) => {
     transaction.delete(memberDocRef);
   });
 }
 
-export const assignTeamRole = async (teamId, memberId, role) => {
+export const assignTeamRole = async (teamId: string, memberId: string, role: string): Promise<void> => {
   const memberDocRef = doc(db, 'teams', teamId, 'members', memberId);
   await updateDoc(memberDocRef, { role });
 }
 
-export const deleteTeam = async (teamId) => {
+export const deleteTeam = async (teamId: string): Promise<void> => {
   const teamDocRef = doc(teams, teamId);
   await runTransaction(db, async (transaction) => {
     transaction.delete(teamDocRef);

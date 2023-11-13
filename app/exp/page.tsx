89c160954f6b52d9c725938c174d3ff24d7d9172
abrarabedin001 <abrarabedin001@gmail.com>
2023-11-13 22:19:38 +0600

    assigning work to member o done

--- app/exp/page.tsx
+++ app/exp/page.tsx
@@ -6,17 +6,17 @@ import React from 'react'
 export default function ExpPage() {
   const adminId = 'p1nixi24VsW1AteaGeOHTcOeJVF3' // Replace with actual admin ID
 
-  const handleTeamSubmit = async (teamDetails: any) => {
-    console.log('Team Details:', teamDetails)
-    // addSampleData()
-    // Here, you can call the createTeam function to save the team details to your database
-    try {
-      const teamId = await createTeam(teamDetails.name, teamDetails.adminId)
-      console.log('Team Created with ID:', teamId)
-    } catch (error) {
-      console.error('Error creating team:', error)
-    }
-  }
+  // const handleTeamSubmit = async (teamDetails: any) => {
+  //   console.log('Team Details:', teamDetails)
+  //   // addSampleData()
+  //   // Here, you can call the createTeam function to save the team details to your database
+  //   try {
+  //     const teamId = await createTeam(teamDetails.name, teamDetails.adminId)
+  //     console.log('Team Created with ID:', teamId)
+  //   } catch (error) {
+  //     console.error('Error creating team:', error)
+  //   }
+  // }
   return (
     <div>
       <h1 style={{ textAlign: 'center' }}>Create Team</h1>

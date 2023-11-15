7eaacd44f972f1a4a4b359b5ddbeb7c96fc31504
anikmd445 <anikmd445@gmail.com>
2023-11-15 17:24:10 +0600

    create team routes

---
+++ app/api/teams/route.ts
@@ -0,0 +1,29 @@
+import { createTeam } from "@/Controller/firestore/firebaseDb"
+import user from "@/lib/token"
+
+
+
+export async function POST(request: Request) {
+  console.log(user)
+
+  try {
+    if (user) {
+      console.log(user)
+      const body = await request.json()
+      console.log(body)
+      let res = await createTeam('anik','FHSftPiZ4jOvZhcaFE7jvjUHuDg1','messi')
+      let something = JSON.stringify(res)
+      return Response.json({ "hello": something })
+
+    }
+    return Response.json({ message: 'Success' });
+  } catch (error) {
+    console.error('Failed to create team', error);
+    return Response.json({ error: 'Internal Server Error' });
+  }
+}
+export async function GET(req: Request) {
+  const data = await req.json();
+  console.log(data);
+  return new Response("Hello, Next.js!");
+}

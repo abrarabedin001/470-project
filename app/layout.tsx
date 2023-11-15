a0733718c172a1ea29430c74a27723936a9e9625
abrarabedin001 <abrarabedin001@gmail.com>
2023-11-15 17:02:59 +0600

    fized the name

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,48 +1,44 @@
-import './globals.css';
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import TopMenu from '../components/TopMenu';
-import Foot from '../components/Foot';
-import { ThemeProvider } from '@/components/theme-provider';
-import TeamSwitcher from './dashboard/components/team-switcher';
-import { UserNav } from './dashboard/components/user-nav';
-import { MainNav } from './dashboard/components/main-nav';
-import { Search } from './dashboard/components/search';
-import AuthWrapper from './AuthWrapper';
-const inter = Inter({ subsets: ['latin'] });
-import { Toaster } from "@/components/ui/toaster"
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import TopMenu from '../components/TopMenu'
+import Foot from '../components/Foot'
+import { ThemeProvider } from '@/components/theme-provider'
+import TeamSwitcher from './dashboard/components/team-switcher'
+import { UserNav } from './dashboard/components/user-nav'
+import { MainNav } from './dashboard/components/main-nav'
+import { Search } from './dashboard/components/search'
+import AuthWrapper from './AuthWrapper'
+const inter = Inter({ subsets: ['latin'] })
+import { Toaster } from '@/components/ui/toaster'
 export const metadata: Metadata = {
-  title: 'CleverCore',
-  description: 'Automate, Analyze, and Amplify your Communication Processes',
-};
+  title: 'BugSmasher',
+  description: 'BugSmasher is a bug tracking tool for developers.',
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-     
-      <ThemeProvider 
-      attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-            >
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
         <AuthWrapper>
           <body className="  gradient-container  w-full flex flex-col justify-center items-center ">
             <TopMenu />
-          
-          {children}
+
+            {children}
 
             <Foot />
           </body>
-        
         </AuthWrapper>
-      
       </ThemeProvider>
-      
     </html>
-  );
+  )
 }

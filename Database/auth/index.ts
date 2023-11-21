c4286573ba857e84d06b3f900dce916fce574f47
anikmd445 <anikmd445@gmail.com>
2023-11-21 14:36:36 +0600

    forget password

--- Database/auth/index.ts
+++ Database/auth/index.ts
@@ -9,11 +9,13 @@ import {
   signOut,
   User,
   createUserWithEmailAndPassword,
-
+  sendPasswordResetEmail,
+  deleteUser,
   // updateProfile,
 } from 'firebase/auth'
 import { ref, uploadBytes, getDownloadURL, getStorage } from 'firebase/storage'
 export const auth = getAuth(firebase_app)
+
 import { db } from '../firestore/firebaseDb'
 import {
   doc,
@@ -118,9 +120,87 @@ export async function getUserCustomClaims() {
   return res
 }
 
+export async function resetPassword(email: string) {
+  let result = null,
+    error = null;
+  try {
+    await sendPasswordResetEmail(auth, email);
+    result = 'Password reset email sent!';
+  } catch (e) {
+    error = e as FirebaseError;
+  }
+  return { result, error };
+}
+
+const user1 = auth.currentUser;
+export async function checkEmailInDatabase(email: string) {
+  let result=0
+  try {
+    console.log(email)
+    await createUserWithEmailAndPassword(auth, email, '123456')
+        .then(function(user) {
+          if ( user1 != null ){
+            deleteUser(user1).then(() => {
+              // User deleted.
+              console.log("delete hoise")
+            }).catch((error) => {
+              // An error ocurred
+              // ...
+            });
+          }
+          // Email doesn't exist, you can handle this case
+          console.log('Email does not exist');
+          result=0;
+        })
+        .catch (async function(error) {
+                if (error.code === 'auth/email-already-in-use') {
+                   console.log('hello vai')
+                  // Email already exists, handle this case
+                  result = 1;
+                  console.log('Email already exists');
+                } else {
+                  // Handle other errors
+                  console.error(error.message);
+                }
+              });
+             
+
+  } catch (error) {
+    console.error('Error checking email in database:', error);
+    throw error as FirebaseError;
+  }
+  return { result };
+}
 
 
 
+
+// export async function resetPassword(email: string) {
+//   let result = null,
+//     error = null;
+//   try {
+//     await createUserWithEmailAndPassword(auth, email, '123456')
+//     .then(function(user) {
+//       // Email doesn't exist, you can handle this case
+//       console.log('Email does not exist');
+//     })
+//     .catch (async function(error) {
+//       if (error.code === 'auth/email-already-in-use') {
+//          console.log('hello vai')
+//         // Email already exists, handle this case
+//         await sendPasswordResetEmail(auth, email);
+//         result = 'Password reset email sent!';
+//         console.log('Email already exists');
+//       } else {
+//         // Handle other errors
+//         console.error(error.message);
+//       }
+//     });
+//   } catch (e) {
+//     error = e as FirebaseError;
+//   }
+//   return { result, error };
+// }
 // export const updateUserName = async (userName: string) => {
 //   const user = auth.currentUser
 //   if (user) {

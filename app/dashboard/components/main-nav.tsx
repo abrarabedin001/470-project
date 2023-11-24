c3fdfc9f53a9c13e5bf6bfdacd32d109096b2f48
abrarabedin001 <abrarabedin001@gmail.com>
2023-11-24 19:56:47 +0600

    removed stuff from menu

--- app/dashboard/components/main-nav.tsx
+++ app/dashboard/components/main-nav.tsx
@@ -1,43 +1,29 @@
-import Link from "next/link"
+import Link from 'next/link'
 
-import { cn } from "@/lib/utils"
+import { cn } from '@/lib/utils'
+import { useUserStore } from '@/Store/userStore'
 
 export function MainNav({
   className,
   ...props
 }: React.HTMLAttributes<HTMLElement>) {
+  let currrentTeam = useUserStore((state) => state.currrentTeam)
   return (
     <nav
-      className={cn("flex w-full items-center space-x-4 lg:space-x-6", className)}
+      className={cn(
+        'flex w-full items-center space-x-4 lg:space-x-6',
+        className
+      )}
       {...props}
     >
-      <Link
-        href="/dashboard"
-        className="text-sm font-medium transition-colors hover:text-primary"
-      >
-        Overview
-      </Link>
-      <Link
-        href="/examples/dashboard"
-        // className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
-        className="text-sm font-medium transition-colors hover:text-primary"
-      >
-        Customers
-      </Link>
-      <Link
-        href="/examples/dashboard"
-        // className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
-        className="text-sm font-medium transition-colors hover:text-primary"
-      >
-        Products
-      </Link>
-      <Link
-        href="/settings"
-        // className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
-        className="text-sm font-medium transition-colors hover:text-primary"
-      >
-        Settings
-      </Link>
+      {currrentTeam && (
+        <Link
+          href="/dashboard"
+          className="text-sm font-medium transition-colors hover:text-primary"
+        >
+          Dashboard
+        </Link>
+      )}
     </nav>
   )
 }

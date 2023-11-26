8d8bc27a7da554de03cb3a33a78b474ad18e5669
anikmd445 <anikmd445@gmail.com>
2023-11-26 13:07:51 +0600

    task count done

---
+++ app/dashboard/components/countTasks.tsx
@@ -0,0 +1,31 @@
+// count.tsx
+
+import React, { useEffect, useState } from 'react';
+import { DataTable } from '@/app/dashboard/tasks/components/data-table';
+import { columns } from '@/app/dashboard/tasks/components/columns';
+import { getAllTasksInTeam } from '@/Database/firestore/firebaseDb';
+import { useUserStore } from '@/Store/userStore';
+
+export default function CountTasks() {
+  const teamId: any = useUserStore((state) => state.currrentTeam?.value);
+  const [tasks, setTasks] = useState<any[]>([]);
+
+  useEffect(() => {
+    if (teamId) {
+        getAllTasksInTeam(teamId).then((res) => {
+        setTasks(res);
+        console.log('tasks', tasks);
+      });
+    } else {
+      setTasks([]);
+    }
+  }, [teamId]);
+
+  return (
+    <>
+          <p className="text-center font-medium">
+            {tasks.length}
+          </p>
+    </>
+  );
+}

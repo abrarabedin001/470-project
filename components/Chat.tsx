d8d812d262c1ccd2a69d6cb75d856b31ff0b641a
Shahadat-Ahmed <shahadatahamed364375@gmail.com>
2023-11-26 17:16:09 +0600

    dfdfd

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -1,75 +1,196 @@
-import React, { useEffect, useRef } from 'react'
-import { Input } from '@/components/ui/input'
-import { Button } from './ui/button'
-import { useUserStore } from '@/Store/userStore'
-import { addMessage, getChatMessages } from '@/Database/firestore/firebaseDb'
+// import React, { useEffect, useRef } from 'react'
+// import { Input } from '@/components/ui/input'
+// import { Button } from './ui/button'
+// import { useUserStore } from '@/Store/userStore'
+// import { addMessage, getChatMessages } from '@/Database/firestore/firebaseDb'
+
+// export default function Chat() {
+//   const [message, setMessage] = React.useState('')
+//   const teamId = useUserStore((state) => state.currrentTeam?.value)
+//   const teamMembers = useUserStore((state) => state.teamMembers)
+//   const userId = useUserStore((state) => state.user?.uid)
+//   const [messageList, setMessageList] = React.useState<any[]>([])
+
+//   const chatContainerRef = useRef(null)
+
+//   const fetchMessages = async () => {
+//     if (teamId) {
+//       let list = await getChatMessages(teamId)
+//       setMessageList(list)
+//       console.log('MessageList:', list)
+//     }
+//   }
+
+//   useEffect(() => {
+//     fetchMessages()
+//   }, [])
+
+//   const retEmail = (id: string) => {
+//     let ret = teamMembers!.filter((member) => member.id == id)[0].email
+//     console.log('ret:', ret)
+//     return ret
+//   }
+
+//   useEffect(() => {
+//     // Scroll to the bottom whenever the messageList is updated
+//     scrollChatToBottom()
+//   }, [messageList])
+
+//   const handleSendMessage = () => {
+//     if (teamId && message != '') {
+//       addMessage(teamId, userId!, message).then(() => {
+//         fetchMessages()
+//       })
+//     }
+//     setMessage('')
+//   }
+
+//   const scrollChatToBottom = () => {
+//     if (chatContainerRef.current) {
+//       // @ts-ignore
+//       chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight
+//     }
+//   }
+
+//   return (
+//     <div className="w-[650px] border border-gray-700 px-5 rounded-lg mx-auto mb-6">
+//       <div
+//         className="flex flex-col h-[240px] overflow-y-auto"
+//         ref={chatContainerRef}
+//       >
+//         {messageList.map((item) => {
+//           const className = `chat-message ${
+//             item.userId === userId ? 'right text-right' : 'left text-left'
+//           }`
+//           console.log(className) // Log class name to check
+//           return (
+//             <div key={item.id} className={className + ' flex flex-col'}>
+//               <p className="text-[7px]">{retEmail(item.userId)}</p>
+//               <p className="text-white">{item.text}</p>
+//               <p className="text-[7px]">{String(item.createdAt)}</p>
+//             </div>
+//           )
+//         })}
+//       </div>
+
+//       <div className="flex flex-row pt-5 justify-between mb-2">
+//         <Input
+//           placeholder="Type here..."
+//           value={message}
+//           onChange={(e) => setMessage(e.target.value)}
+//         />
+//         <Button className="ml-5" onClick={handleSendMessage} title="Send">
+//           Send
+//         </Button>
+//       </div>
+//     </div>
+//   )
+// }
+
+
+
+
+
+import React, { useEffect, useRef } from 'react';
+import md5 from 'md5';
+import { Input } from '@/components/ui/input';
+import { Button } from './ui/button';
+import { useUserStore } from '@/Store/userStore';
+import { addMessage, getChatMessages } from '@/Database/firestore/firebaseDb';
 
 export default function Chat() {
-  const [message, setMessage] = React.useState('')
-  const teamId = useUserStore((state) => state.currrentTeam?.value)
-  const teamMembers = useUserStore((state) => state.teamMembers)
-  const userId = useUserStore((state) => state.user?.uid)
-  const [messageList, setMessageList] = React.useState<any[]>([])
+  const [message, setMessage] = React.useState('');
+  const teamId = useUserStore((state) => state.currrentTeam?.value);
+  const teamMembers = useUserStore((state) => state.teamMembers);
+  const userId = useUserStore((state) => state.user?.uid);
+  const [messageList, setMessageList] = React.useState<any[]>([]);
 
-  const chatContainerRef = useRef(null)
+  const chatContainerRef = useRef(null);
 
   const fetchMessages = async () => {
     if (teamId) {
-      let list = await getChatMessages(teamId)
-      setMessageList(list)
-      console.log('MessageList:', list)
+      let list = await getChatMessages(teamId);
+      setMessageList(list);
+      console.log('MessageList:', list);
     }
-  }
+  };
 
   useEffect(() => {
-    fetchMessages()
-  }, [])
+    fetchMessages();
+  }, []);
 
   const retEmail = (id: string) => {
-    let ret = teamMembers!.filter((member) => member.id == id)[0].email
-    console.log('ret:', ret)
-    return ret
-  }
+    let ret = teamMembers!.filter((member) => member.id === id)[0].email;
+    console.log('ret:', ret);
+    return ret;
+  };
 
   useEffect(() => {
     // Scroll to the bottom whenever the messageList is updated
-    scrollChatToBottom()
-  }, [messageList])
+    scrollChatToBottom();
+  }, [messageList]);
 
   const handleSendMessage = () => {
-    if (teamId && message != '') {
+    if (teamId && message !== '') {
       addMessage(teamId, userId!, message).then(() => {
-        fetchMessages()
-      })
+        fetchMessages();
+      });
     }
-    setMessage('')
-  }
+    setMessage('');
+  };
 
   const scrollChatToBottom = () => {
     if (chatContainerRef.current) {
       // @ts-ignore
-      chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight
+      chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
     }
-  }
+  };
+
+  // Function to create an avatar from the first two letters of the email
+  const getAvatarFromEmail = (email: string) => {
+    const initials = email.substring(0, 2).toUpperCase();
+    return (
+      <div className="avatar">
+        {initials}
+      </div>
+    );
+  };
+
+  // Function to format the timestamp
+  const formatTimestamp = (timestamp: number) => {
+    const date = new Date(timestamp);
+    const formattedDate = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
+    const formattedTime = `${date.getHours()}:${String(date.getMinutes()).padStart(2, '0')}`;
+    return `${formattedDate} ${formattedTime}`;
+  };
 
   return (
     <div className="w-[650px] border border-gray-700 px-5 rounded-lg mx-auto mb-6">
-      <div
-        className="flex flex-col h-[240px] overflow-y-auto"
-        ref={chatContainerRef}
-      >
+      <div className="flex flex-col h-[240px] overflow-y-auto" ref={chatContainerRef}>
         {messageList.map((item) => {
-          const className = `chat-message ${
-            item.userId === userId ? 'right text-right' : 'left text-left'
-          }`
-          console.log(className) // Log class name to check
+          const isCurrentUser = item.userId === userId;
+          const messageContainerClass = `chat-message-container ${
+            isCurrentUser ? 'current-user' : 'other-user'
+          }`;
+
           return (
-            <div key={item.id} className={className + ' flex flex-col'}>
-              <p className="text-[7px]">{retEmail(item.userId)}</p>
-              <p className="text-white">{item.text}</p>
-              <p className="text-[7px]">{String(item.createdAt)}</p>
+            <div key={item.id} className={messageContainerClass}>
+              <div className="message-header">
+                {getAvatarFromEmail(retEmail(item.userId))}
+                <div>
+                  <p style={{ fontSize: '10px', fontWeight: 'bold', marginBottom: '2px' }}>
+                    {retEmail(item.userId)}
+                  </p>
+                  <p style={{ fontSize: '10px', color: 'white' /*color: '#72767d'*/ }}>
+                    {formatTimestamp(item.createdAt.toDate().getTime())}
+                  </p>
+                </div>
+              </div>
+              <div className="message-content">
+                <p style={{ fontSize: '14px', color: '#ffffff' }}>{item.text}</p>
+              </div>
             </div>
-          )
+          );
         })}
       </div>
 
@@ -84,5 +205,5 @@ export default function Chat() {
         </Button>
       </div>
     </div>
-  )
+  );
 }

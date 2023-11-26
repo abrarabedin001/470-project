3123bcc5e6af21c29eb6a209a4560fb19654ed2d
anikmd445 <anikmd445@gmail.com>
2023-11-27 04:31:30 +0600

    table fixed

--- app/dashboard/components/overview.tsx
+++ app/dashboard/components/overview.tsx
@@ -25,43 +25,44 @@ export default function OverviewPage() {
     <>
       {/* <div className="flex h-full flex-1 flex-col p-8 overflow-y-auto"> */}
       <div className="card px-4 pb-4 max-h-[250px] overflow-y-auto custom-scrollbar rounded-2xl">
-        <table className="owcustom-table-white dark:owcustom-table text-gray-700 dark:text-silver-700 rounded-2xl">
-          <thead className="">
-            <tr className="rounded-xl">
-              <th className="owtable-header-light dark:owtable-header custom-head-1">
-                Title
-              </th>
-              <th className="owtable-header-light dark:owtable-header">
-                Priority
-              </th>
-              <th className="owtable-header-light dark:owtable-header ">
-                Status
-              </th>
-              <th className="owtable-header-light dark:owtable-header custom-head-2">
-                CreatedAt
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {tasks.map((task, index) => (
-              <tr key={index} className="border divide-y divide-solid divide-x">
-                <td className="text-center owtable-body text-black dark:text-white truncate-custom">
-                  {task.title}
-                </td>
-                <td className="text-center owtable-body text-black dark:text-white ">
-                  {task.priority}
-                </td>
-                <td className="text-center owtable-body text-black dark:text-white">
-                  {task.status}
-                </td>
-                <td className="text-center owtable-body text-black dark:text-white">
-                  {task.createdAt.toLocaleString()}
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+  <table className="owcustom-table-white dark:owcustom-table text-gray-700 dark:text-silver-700 rounded-2xl">
+    <thead className="">
+      <tr className="rounded-xl">
+        <th className="owtable-header-light dark:owtable-header custom-head-1" style={{ width: '32%' }}>
+          Title
+        </th>
+        <th className="owtable-header-light dark:owtable-header" style={{ width: '16%' }}>
+          Priority
+        </th>
+        <th className="owtable-header-light dark:owtable-header" style={{ width: '18%' }}>
+          Status
+        </th>
+        <th className="owtable-header-light dark:owtable-header custom-head-2" style={{ width: '34%' }}>
+          CreatedAt
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      {tasks.map((task, index) => (
+        <tr key={index} className="border divide-y divide-solid divide-x">
+          <td className="text-center owtable-body text-black dark:text-white" style={{ width: '32%', wordBreak: 'break-all', overflowWrap: 'break-word'}}>
+            {task.title}
+          </td>
+          <td className="text-center owtable-body text-black dark:text-white" style={{ width: '16%' }}>
+            {task.priority}
+          </td>
+          <td className="text-center owtable-body text-black dark:text-white" style={{ width: '18%' }}>
+            {task.status}
+          </td>
+          <td className="text-center owtable-body text-black dark:text-white" style={{ width: '34%' }}>
+            {task.createdAt.toLocaleString()}
+          </td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+</div>
+
       {/* </div> */}
     </>
   )

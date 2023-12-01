22967ad23d543390ff5b018f7d6eb66df5d3d3a2
abrarabedin001 <abrarabedin001@gmail.com>
2023-12-01 20:23:46 +0600

    .

--- app/dashboard/tasks/components/columns.tsx
+++ app/dashboard/tasks/components/columns.tsx
@@ -1,11 +1,8 @@
 'use client'
 
 import { ColumnDef } from '@tanstack/react-table'
-
 import { Badge } from '@/components/ui/badge'
-
 import { Checkbox } from '@/components/ui/checkbox'
-
 import { labels, priorities, statuses } from '../data/data'
 import { Task } from '../data/schema'
 import { DataTableColumnHeader } from './data-table-column-header'
@@ -79,8 +76,6 @@ export const columns: ColumnDef<Task>[] = [
 
       return (
         <div className="flex space-x-2">
-          {/* {label && <Badge variant="outline">{label.label}</Badge>} */}
-
           <Popover>
             <PopoverTrigger>
               <Button variant="outline">
